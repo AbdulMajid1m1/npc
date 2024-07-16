8097908f8be362339a0509b01e3dfa8c8aa2235e
Eissanoor <eissaanoor@gmail.com>
2024-07-16 16:35:10 +0500

    eissa noor

--- backend/routes/RootRoute.js
+++ backend/routes/RootRoute.js
@@ -2,8 +2,9 @@ import express from "express";
 const router = express.Router();
 
 import userRoutes from "./subRoutes/usersRoutes.js";
+import masterData from "./EissaRootRoute.js";
 
-
+router.use("/master-data", masterData);
 router.use("/users", userRoutes);
 
 export default router;
