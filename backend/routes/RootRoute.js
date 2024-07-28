cb0621fc7c31cc2001042b5cbcd546fd06327728
Eissanoor <eissaanoor@gmail.com>
2024-07-28 13:34:48 +0500

    updated

--- backend/routes/RootRoute.js
+++ backend/routes/RootRoute.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 import userRoutes from "./subRoutes/usersRoutes.js";
 import masterData from "./EissaRootRoute.js";
-
+import cataloges from "./subRoutes/calatoge.js"
 router.use("/master-data", masterData);
 router.use("/users", userRoutes);
-
+router.use("/", cataloges);
 export default router;
