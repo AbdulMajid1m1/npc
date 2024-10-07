1465e8190e286f677e5932d1ac844d7dab253069
unknown <abdulmajid1m2@gmail.com>
2024-10-07 11:58:40 +0500

    as

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -4,6 +4,7 @@ export const phpUrl = "https://gs1ksa.org/api";
 // export const baseUrlnpc = "http://localhost:3082/api";
 export const baseUrlnpc = "http://gs1ksa.org:3082/api";
 
-export const backendUrl = "http://localhost:3082";
+// export const backendUrl = "http://localhost:3082";
+export const backendUrl = "http://gs1ksa.org:3082";
 
 export const vectorEmbeddingUrl = "https://gs1ksa.org:4044/api";
