1b67b2ac6524618239f302413a8b1b8954b4f717
HasnainAhmad0022 <hasnainahmad0022@gmail.com>
2024-10-07 11:56:03 +0500

    Commit

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -7,6 +7,4 @@ export const baseUrlnpc = "http://gs1ksa.org:3082/api";
 // export const backendUrl = 'http://localhost:3082';
 export const backendUrl = 'http://gs1ksa.org:3082';
 
-export const vectorEmbeddingUrl = 'https://gs1ksa.org:4044/api';
-
 
